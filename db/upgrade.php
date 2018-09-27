add871d8b4ea12005aed7f3372112aec92fc48f6
LTS <github@lts.ie>
2018-09-27 15:10:32 +0000

    v1.5

---
+++ db/upgrade.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * tool_kholland plugin upgrade code
+ *
+ * @package    tool_kholland
+ * @copyright  2018 Karen Holland
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Function to upgrade tool_kholland.
+ *
+ * @param int $oldversion the version we are upgrading from
+ * @return bool result
+ */
+function xmldb_tool_kholland_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2018091900) {
+
+        // Define table tool_kholland to be created.
+        $table = new xmldb_table('tool_kholland');
+
+        // Adding fields to table tool_kholland.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, null, null, null);
+        $table->add_field('completed', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('priority', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '1');
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table tool_kholland.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for tool_kholland.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Kholland savepoint reached.
+        upgrade_plugin_savepoint(true, 2018091900, 'tool', 'kholland');
+    }
+
+    if ($oldversion < 2018091902) {
+
+        // Define table tool_kholland to be created.
+        $table = new xmldb_table('tool_kholland');
+
+        $key = new xmldb_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'course', array('id'));
+
+        // Launch add key courseid.
+        $dbman->add_key($table, $key);
+
+        $index = new xmldb_index('test', XMLDB_INDEX_UNIQUE, array('courseid', 'name'));
+
+        // Conditionally launch add index test.
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+
+        // Kholland savepoint reached.
+        upgrade_plugin_savepoint(true, 2018091902, 'tool', 'kholland');
+    }
+
+
+    return true;
+}
--- version.php
+++ version.php
@@ -27,4 +27,4 @@ $plugin->requires = 2018051700;
 $plugin->cron = 0;
 $plugin->component = 'tool_kholland';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.4';
+$plugin->release = '1.5';
